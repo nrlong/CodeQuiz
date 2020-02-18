59d4f778a76075571dd302f7e58a9c396f8aade4
Nathan Long <nrlong13@gmail.com>
2020-02-18 13:12:25 -0500

    completed JS file with questions array for quiz

---
+++ assets/questions.js
@@ -0,0 +1,51 @@
+const myQuestions = [{
+    question: "What is the HTML tag under which one can write JavaScript Code?",
+    answers:{
+    a: "<javascript>",
+    b: "<scripted>",
+    c: "<script>",
+    d: "<js>"
+    },
+    correctAnswer: "c"
+},
+{
+    question: "Which is the correct syntax to display an alert?",
+    answers: {
+        a: "alertbox('alert')",
+        b: "msg('alert')",
+        c: "msgbox('alert')",
+        d: "alert('alert')",
+    },
+    correctAnswer: "d"
+},
+{
+    question: "The external JavaScript file must contain <script> tag.  True or False?",
+    answers: {
+        a: "True",
+        b: "False"
+    },
+    correctAnswer: "b"
+},
+
+{
+    question: "How is a function called in JavaScript?",
+    answers: {
+        a: "call someFunction();",
+        b: "call function someFunction();",
+        c: "someFunction();",
+        d: "function someFunction();"
+    },
+    correctAnswer: "c"
+},
+
+{
+    question: "Who invented JavaScript?",
+    answers: {
+        a: "Douglas Crockford",
+        b: "Bill Gates",
+        c: "Starbucks",
+        d: "Brendan Eich"
+    },
+    correctAnswer: "d"
+}
+];
---
+++ assets/script.js
@@ -0,0 +1,23 @@
+let quizContainer = $("questions");
+let resultsContainer = $("gameover");
+let submitButton = $("submit");
+
+
+//Function to build the quiz
+function buildQuiz(){
+
+}
+
+
+
+//function to show the results
+function showResults(){
+
+}
+
+//output
+buildQuiz();
+
+
+//submit button
+submitButton.add("click", showResults);
