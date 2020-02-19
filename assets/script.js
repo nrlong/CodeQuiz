34a85e5928ce461757fd735906d8c153d6b7f8af
Nathan Long <nrlong13@gmail.com>
2020-02-19 14:45:10 -0500

    added functions to JS to start building script for quiz output.  build quiz function complete.  question output WIP

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,46 @@
 let quizContainer = $("questions");
 let resultsContainer = $("gameover");
 let submitButton = $("submit");
+let timeLink = $("time");
+let time = myQuestions.length * 15;
 
+let startBtn = $("start");
+let timer;
 
 //Function to build the quiz
 function buildQuiz(){
+   let startScreen = $("start-screen");
+   startScreen.addClass("class", "hide");
 
+   quizContainer.removeClass();
+
+   timer = setInterval(clockTick, 1000);
+
+   timeLink.textContent = time;
+
+   getQuestions();
 }
 
+function getQuestion(){
 
+    
+
+}
 
 //function to show the results
 function showResults(){
 
 }
 
+function clockTick(){
+    time--;
+    timeLink.textContent = time;
+}
+
 //output
 buildQuiz();
 
+startBtn.onclick = buildQuiz;
 
 //submit button
 submitButton.add("click", showResults);
